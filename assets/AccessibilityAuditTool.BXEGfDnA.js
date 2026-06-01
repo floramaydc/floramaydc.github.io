2f94c11537a5401ae6c8781a6c30d6717d4ae56d
floramaydc-m <fdelacruz@microsoft.com>
2026-06-01 13:12:45 -1000

    Fix: sanitize accessibility audit error output

--- assets/AccessibilityAuditTool.BXEGfDnA.js
+++
@@ -1,1 +0,0 @@
-import{A as p}from"./AccessibilityAuditTool.DXipOsK_.js";import"./jsx-runtime.ECMnFLoH.js";import"./index.DuNJ4Zyq.js";export{p as default};
---
+++ assets/AccessibilityAuditTool.QYlMPoKQ.js
@@ -0,0 +1,1 @@
+import{A as p}from"./AccessibilityAuditTool.DhPs9eun.js";import"./jsx-runtime.ECMnFLoH.js";import"./index.DuNJ4Zyq.js";export{p as default};
